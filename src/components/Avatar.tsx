f8c704e8ef3c5f88c358b2c0f31a065672148258
Eugene Oleynikov <eugenefilter@gmail.com>
2025-03-11 23:38:07 +0100

    correct avatar url

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -2,8 +2,8 @@
 export default function Avatar() {
   return (
     <picture>
-      <source type="image/webp" srcSet="./public/avatar.webp" />
-      <img src="./public/avatar.jpg" alt="Photo" className="w-40 h-40 rounded-full mx-auto my-1" />
+      <source type="image/webp" srcSet="avatar.webp" />
+      <img src="avatar.jpg" alt="Photo" className="w-40 h-40 rounded-full mx-auto my-1" />
     </picture>
   )
 }
